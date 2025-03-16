92b6c087cfe18bdd3aa5e4a7714dc715c52c3193
jackloosantamaria <jackloosantamaria@hotmail.com>
2025-03-16 18:25:58 -0500

    removing a few unused line to run vercel

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -36,14 +36,6 @@ export type State = {
 };
 
 export async function createInvoice(prevState: State, formData: FormData){
-    const rawFormData = {
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-      };
-
-      // Test it out:
-      //console.log(rawFormData);
 
       const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
