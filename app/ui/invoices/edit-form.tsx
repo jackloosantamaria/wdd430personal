da41c978ee53d86a3e630b284c906b0728d4b439
jackloosantamaria <jackloosantamaria@hotmail.com>
2025-03-16 19:39:51 -0500

    restoring what was deleted

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,7 +9,7 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
-
+import { UpdateInvoice } from './buttons';
 import { updateInvoice, State } from '@/app/lib/actions'
 import { useActionState } from 'react';
 
