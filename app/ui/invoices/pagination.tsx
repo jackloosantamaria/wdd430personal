03246f88c7cea4e817a2fdea4166845cbceb8a7d
jackloosantamaria <jackloosantamaria@hotmail.com>
2025-03-17 20:44:12 -0500

    comment out pathname and searchparams in pagination

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -10,8 +10,8 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   // NOTE: Uncomment this code in Chapter 11
 
   //const allPages = generatePagination(currentPage, totalPages);
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
+  // const pathname = usePathname();
+  // const searchParams = useSearchParams();
   //const currentPage = Number(searchParams.get('page')) || 1;
 
 
