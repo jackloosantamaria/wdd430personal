329a6d3b4413ba6d1c210ffb1badbcc1e2ed59c8
jackloosantamaria <jackloosantamaria@hotmail.com>
2025-03-17 20:40:15 -0500

    comment out a few lines of pagination

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -12,14 +12,14 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   //const allPages = generatePagination(currentPage, totalPages);
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  const currentPage = Number(searchParams.get('page')) || 1;
+  //const currentPage = Number(searchParams.get('page')) || 1;
 
 
-  const createPageURL = (pageNumber: number | string) => {
-    const params = new URLSearchParams(searchParams);
-    params.set('page', pageNumber.toString());
-    return `${pathname}?${params.toString()}`;
-  };
+  // const createPageURL = (pageNumber: number | string) => {
+  //   const params = new URLSearchParams(searchParams);
+  //   params.set('page', pageNumber.toString());
+  //   return `${pathname}?${params.toString()}`;
+  // };
   
   return (
     <>
@@ -63,36 +63,36 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   );
 }
 
-function PaginationNumber({
-  page,
-  href,
-  isActive,
-  position,
-}: {
-  page: number | string;
-  href: string;
-  position?: 'first' | 'last' | 'middle' | 'single';
-  isActive: boolean;
-}) {
-  const className = clsx(
-    'flex h-10 w-10 items-center justify-center text-sm border',
-    {
-      'rounded-l-md': position === 'first' || position === 'single',
-      'rounded-r-md': position === 'last' || position === 'single',
-      'z-10 bg-blue-600 border-blue-600 text-white': isActive,
-      'hover:bg-gray-100': !isActive && position !== 'middle',
-      'text-gray-300': position === 'middle',
-    },
-  );
+// function PaginationNumber({
+//   page,
+//   href,
+//   isActive,
+//   position,
+// }: {
+//   page: number | string;
+//   href: string;
+//   position?: 'first' | 'last' | 'middle' | 'single';
+//   isActive: boolean;
+// }) {
+//   const className = clsx(
+//     'flex h-10 w-10 items-center justify-center text-sm border',
+//     {
+//       'rounded-l-md': position === 'first' || position === 'single',
+//       'rounded-r-md': position === 'last' || position === 'single',
+//       'z-10 bg-blue-600 border-blue-600 text-white': isActive,
+//       'hover:bg-gray-100': !isActive && position !== 'middle',
+//       'text-gray-300': position === 'middle',
+//     },
+//   );
 
-  return isActive || position === 'middle' ? (
-    <div className={className}>{page}</div>
-  ) : (
-    <Link href={href} className={className}>
-      {page}
-    </Link>
-  );
-}
+//   return isActive || position === 'middle' ? (
+//     <div className={className}>{page}</div>
+//   ) : (
+//     <Link href={href} className={className}>
+//       {page}
+//     </Link>
+//   );
+// }
 
 function PaginationArrow({
   href,
